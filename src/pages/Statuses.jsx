823807a140f4bdea9d302c16b0030db60a7fd28a
tapas-code <tapas.code247@gmail.com>
2023-04-02 01:36:49 +0530

    UI for search-box and filter-options

--- src/pages/Statuses.jsx
+++ src/pages/Statuses.jsx
@@ -31,6 +31,7 @@ export default function Statuses() {
 
   return (
     <div>
+
       <div class="Add_User_Button flex justify-end my-10">
         <label
           onClick={() => setShowStatusModal(true)}
@@ -50,7 +51,31 @@ export default function Statuses() {
           </span>
           Add Status
         </label>
+
       </div>
+
+      <div className="flex justify-between items-center">
+          <div className="filter-box">
+          <select className="select select-primary w-full max-w-xs">
+            <option disabled selected>Order by :-</option>
+            <option>Oldest first</option>
+            <option>Newest first</option>
+          </select>
+          </div>
+
+          {/* search box  */}
+          <div className="search-box">
+            <div className="form-control">
+              <div className="input-group">
+                <input type="text" placeholder="Search…" className="input input-bordered" />
+                <button className="btn btn-square">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
       {/* <div className="my-5">
         <Table refetch={refetch} data={data} />
       </div> */}

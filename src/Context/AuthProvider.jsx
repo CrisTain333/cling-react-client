5feac4dc9c143a38bbb1dbb2c6e04c495d35823a
Sukanta Das <arnobh84@gmail.com>
2023-04-22 14:02:01 +0600

    change in status

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -11,7 +11,7 @@ const AuthProvider = ({ children }) => {
     setIsAuthenticate(false);
     setUser({});
     localStorage.removeItem("accessToken");
-    window.location.reload();
+    // window.location.reload();
   };
 
   return (
